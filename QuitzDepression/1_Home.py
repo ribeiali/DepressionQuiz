a77b16d33ab199ca28a5eb13db4730e90b984129
ribeiali <ribeiali@students.zhaw.ch>
2023-05-31 20:58:46 +0200

    Update 1_Home.py

--- QuitzDepression/1_Home.py
+++ QuitzDepression/1_Home.py
@@ -28,6 +28,47 @@ fullname, authentication_status, username = authenticator.login('Login', 'main')
 
 if authentication_status == True:   # login successful
     authenticator.logout('Logout', 'main')   # show logout button
+
+
+    st.set_page_config(
+        page_title="Home",
+        page_icon="🏠",
+    )
+    st.sidebar.header("© 2023")
+    st.sidebar.markdown("`👩‍💻 Power by Alina, Amine and Vera with Streamlit`")
+
+    # Title and subtitle 
+
+    st.write("# Depression quiz")
+
+
+    st.markdown("""
+        With this study we would like to find out how many women suffer from postnatal depression in Switzerland. 
+        
+        In the following quiz you will be provided with 10 questions: please, tell the truth. Depression is not a joke.
+        Stay Safe.
+        
+    """)
+
+    # button for starting the Quiz 
+    if st.button("📝 Go to quiz"):
+        webbrowser.open_new_tab('https://ribeiali-depressionquiz-quitzdepression1-home-yfd81a.streamlit.app/Quiz')
+
+    #st.image("./media/images/depression1.png")
+
+    # Footer
+
+    st.write("""
+    <footer style='background-color: rgb(51,51,51); width:100%; right:0px; left:0px; bottom:0px; position:fixed'>
+    <div style='padding: 30px; display: block; color:white; width:100%'>
+        <p style='font-size:20px;'>Alina Ribeiro Pinto – Amine Aksu – Vera Gomez</p>
+        <hr style='width:25%; height:3px; background-color:rgb(238,32,121); margin:2px;'>
+
+    </div>
+
+    </footer>
+    """,unsafe_allow_html=True)
+
 elif authentication_status == False:
     st.error('Username/password is incorrect')
     st.stop()
@@ -36,48 +77,3 @@ elif authentication_status == None:
     st.stop()
 
 # Function for Title, Page Icon and Sidebar
-
-st.set_page_config(
-    page_title="Home",
-    page_icon="🏠",
-)
-st.sidebar.header("© 2023")
-st.sidebar.markdown("`👩‍💻 Power by Alina, Amine and Vera with Streamlit`")
-
-# Title and subtitle 
-
-st.write("# Depression quiz")
-
-
-st.markdown("""
-    With this study we would like to find out how many women suffer from postnatal depression in Switzerland. 
-    
-    In the following quiz you will be provided with 10 questions: please, tell the truth. Depression is not a joke.
-    Stay Safe.
-    
-""")
-
-# button for starting the Quiz 
-
-
-
-if st.button("📝 Go to quiz"):
-    webbrowser.open_new_tab('https://ribeiali-depressionquiz-quitzdepression1-home-yfd81a.streamlit.app/Quiz')
-
-
-
-  
-#st.image("./media/images/depression1.png")
-
-# Footer
-
-st.write("""
-<footer style='background-color: rgb(51,51,51); width:100%; right:0px; left:0px; bottom:0px; position:fixed'>
-<div style='padding: 30px; display: block; color:white; width:100%'>
-    <p style='font-size:20px;'>Alina Ribeiro Pinto – Amine Aksu – Vera Gomez</p>
-    <hr style='width:25%; height:3px; background-color:rgb(238,32,121); margin:2px;'>
-
-</div>
-
-</footer>
-""",unsafe_allow_html=True)
