7216b595777f2b7ccee35b87437e53724cf596f9
ribeiali <ribeiali@students.zhaw.ch>
2023-05-31 19:49:54 +0200

    Update 2_Quiz.py
    background

--- QuitzDepression/pages/2_Quiz.py
+++ QuitzDepression/pages/2_Quiz.py
@@ -27,31 +27,31 @@ import base64
 st.sidebar.header("© 2023")
 st.sidebar.markdown("`👩‍💻 Power by Alina, Amine and Vera with Streamlit`")
   
-def get_base64(bin_file):
+# def get_base64(bin_file):
     
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+#     with open(bin_file, 'rb') as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
 
-# Function to display image ad background
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    header{{        
-        opacity: 0;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('media/background/BackEgg.png')
+# # Function to display image ad background
+# def add_bg_from_local(image_file):
+#     with open(image_file, "rb") as image_file:
+#         encoded_string = base64.b64encode(image_file.read())
+#     st.markdown(
+#     f"""
+#     <style>
+#     .stApp {{
+#         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+#         background-size: cover
+#     }}
+#     header{{        
+#         opacity: 0;
+#     }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+#     )
+# add_bg_from_local('media/background/BackEgg.png')
 
 # Define the questions and answer options using a dictionary
 questions = {
