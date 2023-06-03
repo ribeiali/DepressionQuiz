e5ef2e1c267660c130b74f02119741b4f9427720
ribeiali <ribeiali@students.zhaw.ch>
2023-06-03 13:41:44 +0200

    Update 1_Home.py

--- QuitzDepression/1_Home.py
+++ QuitzDepression/1_Home.py
@@ -35,7 +35,7 @@ authenticator = stauth.Authenticate(
 
 fullname, authentication_status, username = authenticator.login('Login', 'main')
 if authentication_status == True:   # login successful
-    col1, col2, col3 = st.beta_columns(3)
+    col1, col2, col3 = st.columns(3)
     with col3:
         authenticator.logout('Logout', 'main')   # show logout button
     
@@ -57,7 +57,7 @@ if authentication_status == True:   # login successful
 
     # button for starting the Quiz 
     if st.button("📝 Go to quiz"):
-        redirect_to_page("https://ribeiali-depressionquiz-quitzdepression1-home-yfd81a.streamlit.app/Quiz");
+        redirect_to_page("https://ribeiali-depressionquiz-quitzdepression1-home-yfd81a.streamlit.app/Quiz")
 
     st.image("/app/depressionquiz/QuitzDepression/media/images/depression1.png")
 
