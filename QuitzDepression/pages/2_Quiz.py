99105df1962202fb0f9129218ddcca9e1d8270e0
ribeiali <ribeiali@students.zhaw.ch>
2023-06-03 15:21:19 +0200

    Update 2_Quiz.py

--- QuitzDepression/pages/2_Quiz.py
+++ QuitzDepression/pages/2_Quiz.py
@@ -159,8 +159,15 @@ if authentication_status == True:   # login successful
     # Display the final score
 
     if st.button('See results'):
-        st.markdown("![Gif Therapy is medicine](../media/images/3.gif)")
+        file_ = open("../media/images/3.gif", "rb")
+        contents = file_.read()
+        data_url = base64.b64encode(contents).decode("utf-8")
+        file_.close()
 
+        st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+            unsafe_allow_html=True,
+        )
         st.session_state['points']=totalScore+point10
         st.session_state['point10']=point10
         st.write("✅ Go to the results page to see the result")
