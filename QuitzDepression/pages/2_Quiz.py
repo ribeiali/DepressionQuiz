e141cc7a85018485a81bef22d32862881a622ea7
ribeiali <ribeiali@students.zhaw.ch>
2023-06-03 15:25:31 +0200

    Update 2_Quiz.py
    removed debug

--- QuitzDepression/pages/2_Quiz.py
+++ QuitzDepression/pages/2_Quiz.py
@@ -159,15 +159,6 @@ if authentication_status == True:   # login successful
     # Display the final score
 
     if st.button('See results'):
-        file_ = open("/app/depressionquiz/QuitzDepression/media/images/3.gif", "rb")
-        contents = file_.read()
-        data_url = base64.b64encode(contents).decode("utf-8")
-        file_.close()
-
-        st.markdown(
-            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-            unsafe_allow_html=True,
-        )
         st.session_state['points']=totalScore+point10
         st.session_state['point10']=point10
         st.write("✅ Go to the results page to see the result")
